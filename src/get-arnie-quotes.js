49f4f4588f9fbbfc16f20eaa387eb92fce698e07
ronitkapoor878 <ronitkapoor878@gmail.com>
2026-01-12 09:13:14 +1100

    Implement getArnieQuotes function to fetch and transform quotes from provided URLs

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,8 +1,20 @@
 const { httpGet } = require('./mock-http-interface');
 
 const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
-  // return results;
+  const requests = urls.map(url => httpGet(url));
+  const responses = await Promise.all(requests);
+  const results = responses.map(response => {
+    const parsedBody = JSON.parse(response.body);
+    const message = parsedBody.message;
+    
+    if (response.status === 200) {
+      return { 'Arnie Quote': message };
+    } else {
+      return { 'FAILURE': message };
+    }
+  });
+  
+  return results;
 };
 
 module.exports = {
